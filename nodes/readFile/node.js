6652e7413690670a112be8231bfad940edc144d7
Rob Halff <rob.halff@gmail.com>
2014-01-04 08:07:17 +0100

    make readFile always return a string instead of buffer

--- nodes/readFile/node.js
+++ nodes/readFile/node.js
@@ -1,1 +1,10 @@
-output = [fs, 'readFile', input.filename, input.options]
+output = function(cb) {
+
+  fs.readFile(input.filename, input.options, function(err, data) {
+    cb({
+      err: err,
+      data: data.toString()
+    });
+  });
+
+};
