87fdb96ea1aef64c6be1b9999c6ec41669edc947
Rob Halff <rob.halff@gmail.com>
2014-01-10 15:09:41 +0100

    fix filename creation

--- nodes/file/node.js
+++ nodes/file/node.js
@@ -1,2 +1,2 @@
 var file = input.ext ? input.name + input.ext : input.name;
-output.file = input.dir ? path.join(input.dir, file) : file;
+output.file = input.dir ? path.normalize([input.dir, file].join(path.sep)) : file;
