030255b870b75c5f3e2e3d91b1d88d3a0a9b63c8
Rob Halff <rob.halff@gmail.com>
2014-01-29 02:02:30 +0100

    simplify readFile

--- nodes/readFile/node.js
+++ nodes/readFile/node.js
@@ -1,15 +1,1 @@
-output = function(cb) {
-
-  fs.readFile(input.filename, {
-      flag: input.flag,
-      encoding: input.encoding
-    }, function(err, data) {
-
-    cb({
-      error: err,
-      data: data.toString()
-    }, done);
-
-  });
-
-};
+output = [fs, 'readFile', input.filename, { flag: input.flag, encoding: input.encoding }];
