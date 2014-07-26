b30f82ed1220467ed010453ff39778ce336c0001
Rob Halff <rob.halff@gmail.com>
2014-07-26 23:13:09 +0200

    update

--- nodes/exists/node.js
+++ nodes/exists/node.js
@@ -1,1 +1,10 @@
-output = [fs, 'exists', input.path]
+on.input.path = function() {
+  fs.exists(data, function(yes) {
+    if(yes) {
+      output( { yes: data } );
+    } else {
+      output( { no: data } );
+    }
+    done();
+  });
+};
